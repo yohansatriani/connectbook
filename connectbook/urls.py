ed2f08d0af05090dbe662a0a3e4eec57668d88ba
Yohanes Dwihastana <yohansatriani@yahoo.co.id>
2019-07-22 00:04:53 +0700

    first commit

---
+++ connectbook/urls.py
@@ -0,0 +1,38 @@
+"""connectbook URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+#login
+from django.contrib.auth import views as auth_views
+
+from connectbook.views import base, home, auth_login, auth_process, auth_logout
+
+
+urlpatterns = [
+    #DJANGO ADMIN
+    path('admin/', admin.site.urls),
+    #BASE
+    path('base/', base, name='base'),
+    #LOGIN
+    path('accounts/login/', auth_login, name='login'),
+    #AUTH
+    path('accounts/auth/', auth_process, name='auth'),
+    #LOGOUT
+    path('accounts/logout/', auth_logout, name='logout'),
+    #BASE
+    path('home/', home, name='home'),
+]
---
+++ connectbook/views.py
@@ -0,0 +1,47 @@
+from django.template.loader import get_template, render_to_string
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Template, Context
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+# Create your views here.
+def base(request):
+    html = render_to_string('page_base.html')
+    return HttpResponse(html)
+
+def auth_login(request):
+    if not request.user.is_authenticated:
+        return render(request, 'account_login.html', {'title': "CB | Login", 'head': "Login"})
+    else:
+        return redirect(home)
+
+def auth_process(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        next_url = request.POST['next']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect(home)
+        else:
+            messages.error(request,'Authentication Failed: Username or password is incorrect')
+            return redirect(auth_login)
+    else:
+        if not request.user.is_authenticated:
+            return redirect(auth_login)
+        else:
+            return redirect(home)
+
+def auth_logout(request):
+    logout(request)
+    return redirect(auth_login)
+
+@login_required()
+def home(request):
+    html = render_to_string('page_home.html', {'title': "Home", 'head': "Home", 'bcitems': [['home', 'Home']]})
+    return HttpResponse(html)
